250c89f9a046195033a9fe7b83796d606eb51046
Mark Barbaric <mark.barbaric@flextrade.com>
2022-11-09 15:51:55 +0000

    Further work on week 1

--- Coursera-NaturalLanguageProcessing/test_process_tweets.py
+++ Coursera-NaturalLanguageProcessing/test_process_tweets.py
@@ -1,5 +1,20 @@
 import text_preprocessing
+import numpy as np
+from nltk import twitter_samples
 
+all_positive_tweets=twitter_samples.strings('positive_tweets.json')
 
-def test_stemming():
-    assert text_preprocessing.is_true() == True
+def test_remove_hyperlinks():
+    tweet1 = "#mbarbari https://this-is-me.com i love this"
+    cleaned_tweet = text_preprocessing.remove_hyperlinks_and_twitter_styles(tweet1)
+    assert cleaned_tweet == "mbarbari  i love this"
+
+def test_tokenize_tweet():
+    tweet1 = "#mbarbari i an loving this website https://this-is-me!!!. True love :) :) ;)"
+    tokenized_tweet = text_preprocessing.process_tweet(tweet1)
+    assert(tokenized_tweet == ['mbarbari', 'love', 'websit', 'true', 'love', ':)', ':)', ';)'])
+
+def test_build_frequecies():
+    tweet1 = all_positive_tweets[0]
+    labels = np.append(np.ones(5), np.zeros(5))
+    freq = text_preprocessing.build_word_frequencies(tweet1, labels)
--- Coursera-NaturalLanguageProcessing/text_preprocessing.py
+++ Coursera-NaturalLanguageProcessing/text_preprocessing.py
@@ -1,2 +1,56 @@
-def is_true():
-    return True
+import re
+import string
+import numpy as np
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import TweetTokenizer
+
+def remove_hyperlinks_and_twitter_styles(tweet):
+    tweet = re.sub(r'\$\w*', '', tweet)
+    # remove old style retweet text "RT"
+    tweet = re.sub(r'^RT[\s]+', '', tweet)
+    # remove hyperlinks    
+    tweet = re.sub(r'https?://[^\s\n\r]+', '', tweet)
+    # remove hashtags
+    # only removing the hash # sign from the word
+    tweet = re.sub(r'#', '', tweet)
+    return tweet
+
+
+def process_tweet(tweet):
+
+    clean_tweet = remove_hyperlinks_and_twitter_styles(tweet)
+    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
+    reduce_len=True)
+    tweet_tokens = tokenizer.tokenize(clean_tweet)
+
+    stemmer = PorterStemmer()
+    stopwords_english = stopwords.words('english')
+
+    tweets_clean = []
+
+    for word in tweet_tokens:
+        if (word not in stopwords_english and
+        word not in string.punctuation):
+            stem_word = stemmer.stem(word)
+            tweets_clean.append(stem_word)
+
+    return tweets_clean  
+
+def build_word_frequencies(tweets, labels):
+    
+    freqs = {}
+    yslist = np.squeeze(labels.to_list())
+
+    for i, tweet in zip(yslist, tweets):
+        for word in process_tweet(tweet):
+            pair = (word, i)
+
+            if pair in freqs:
+                freqs[pair] += 1
+            else:
+                freqs[pair] += 1
+    
+
+    return freqs

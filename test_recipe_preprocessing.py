f168998710cf84adcf7281d2efb4fc92e697bfb0
Mark Barbaric <markantonybarbaric@hotmail.com>
2022-11-13 18:18:43 +0000

    Further work on recipe_preprocessing class to isolate ingredients.

--- test_recipe_preprocessing.py
+++ test_recipe_preprocessing.py
@@ -1,17 +1,13 @@
 
 import recipe_preprocessing
 
-def test_recipe_preprocessing():
-    test_recipe = [
-        '1 x 1kg chicken',
-        'couple cloves of garlic',
-        '2tsp salt',
-        '2tsp pepper',
-        '1 onion',
-        'pinch of basil',
-        '300g carrots',
-        '1 x chicken stock cube'
-    ]
 
-    output = ['chicken',
-    'garlic', 'salt', 'pepper', 'onion', 'basil', 'carrots', 'chicken stock']
+def test_preprocess_word():
+    w1 = "One"
+    assert recipe_preprocessing.preprocess_word(w1) == "1"
+    w2 = "red"    
+    assert recipe_preprocessing.preprocess_word(w2) == "red"
+    w3 = "¼-½"
+    assert recipe_preprocessing.preprocess_word(w3) == "0.25"
+    w3 = "1/4"
+    assert recipe_preprocessing.preprocess_word(w3) == "0.25"

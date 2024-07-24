2b25624a303324507a12e5c228db3ae76b3bb87b
Mark Barbaric <markantonybarbaric@hotmail.com>
2024-07-24 15:44:14 +0100

    tidy_up - renamed Probabilistic_Models course.

--- Natural_Language_Processing_Specialization/Probabilistic_Models/week1/test_minimum_edit_distance.py
+++
@@ -1,8 +0,0 @@
-from minimum_edit_distance import *
-
-def test_passes():
-    assert True
-
-def test_minimum_edit_distance():
-    word1, word2 = 'play', 'stay'
-    assert minimum_edit_distance(word1, word2) == 4
